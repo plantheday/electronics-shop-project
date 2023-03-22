6182a95e2fb745144ae1fbd15e0f2922abb31448
plantheday <plantheday6@gmail.com>
2023-03-22 20:45:04 +0300

    1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,22 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+
+from src.item import Item
+
+
+def test_init():
+    phone = Item("iphone", 100000, 9)
+    assert phone.name == "iphone"
+    assert phone.price == 100000
+    assert phone.quantity == 9
+
+
+def test_calculate_total_price():
+    phone = Item("iphone", 100000, 9)
+    return phone.price
+    assert return(price) == 100000
+
+
+def test_apply_discount():
+    phone = Item("iphone", 100000, 9)
+    assert phone.price == phone.price * phone.pay_rate
