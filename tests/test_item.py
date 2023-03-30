1c4b355a46be1685f3c620a6b4ec3fcd6922a4cf
plantheday <plantheday6@gmail.com>
2023-03-30 03:59:44 +0300

    1

--- tests/test_item.py
+++ tests/test_item.py
@@ -9,14 +9,3 @@ def test_init():
     assert phone.name == "iphone"
     assert phone.price == 100000
     assert phone.quantity == 9
-
-
-def test_calculate_total_price():
-    phone = Item("iphone", 100000, 9)
-    return phone.price
-    assert return(price) == 100000
-
-
-def test_apply_discount():
-    phone = Item("iphone", 100000, 9)
-    assert phone.price == phone.price * phone.pay_rate
